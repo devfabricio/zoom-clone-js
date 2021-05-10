5e114e6cb1d0c76d9c380fe0be59238093b727fe
Fabrício Augusto <fabricio.mata@gmail.com>
2021-05-10 19:27:29 -0300

    feat: create server with socket com connect new users

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -1,9 +1,13 @@
 class Business {
-  constructor({ room, media, view }) {
+  constructor({ room, media, view, socketBuilder }) {
     this.room = room
     this.media = media
     this.view = view
-
+    this.socketBuilder = socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build()
+    this.socketBuilder.emit('join-room', this.room, 'teste01')
     this.currentStream = {}
   }
 
@@ -24,4 +28,15 @@ class Business {
     })
   }
 
+  onUserConnected = function () {
+    return userId => {
+      console.log('user connected!', userId)
+    }
+  }
+
+  onUserDisconnected = function() {
+    return userId => {
+      console.log('user disconnected!', userId)
+    }
+  }
 }
