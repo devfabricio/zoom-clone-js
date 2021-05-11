3cc8939b683adbdbbf09649a7fa57c21a96883ca
Fabrício Augusto <fabricio.mata@gmail.com>
2021-05-11 20:17:55 -0300

    feat: add peer server and configurations

--- public/pages/room/src/view.js
+++ public/pages/room/src/view.js
@@ -22,9 +22,13 @@ class View {
     return video
   }
 
-  renderVideo({ userId, stream = null, url = null, isCurrentId = false }) {
+  renderVideo({ userId, stream = null, url = null, isCurrentId = false, muted= true }) {
     console.log(stream)
-     const video = this.createVideoElement({ src: url, srcObject: stream})
+     const video = this.createVideoElement({
+       muted,
+       src: url,
+       srcObject: stream
+     })
     this.appendToHTMLTree(userId, video, isCurrentId)
   }
 
@@ -41,4 +45,11 @@ class View {
     const videoGrid = document.getElementById('video-grid')
     videoGrid.append(div)
   }
+
+  setParticipants(count) {
+    const myself = 1
+    const participants = document.getElementById('participants')
+    participants.innerHTML = (count + myself)
+  }
+
 }
